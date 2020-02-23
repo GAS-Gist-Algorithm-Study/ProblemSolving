515e5d66afdfa16a632e7cea01587c30ff01716b
Yura Choi <yoorachoi8937@gmail.com>
2020-02-23 20:46:04 +0900

    Add solution to Baekjoon 1708

--- PS_1708_CONVEX_HULL/YRC/1708.cpp
+++ PS_1708_CONVEX_HULL/YRC/1708.cpp
@@ -2,13 +2,8 @@
 # include <vector>
 # include <algorithm>
 # include <stack>
-# include <cmath>
-# define PI 3.14
 
 using namespace std;
-
-typedef pair<int,int> pii;
-typedef pair<int,pii> piii;
 
 class Point{
 public:
@@ -18,31 +13,16 @@ public:
         x = nx;
         y = ny;
     }
-    void setAtan(double atan){
-        angle = atan;
-    }
 };
 
 bool comp (const Point& p1, const Point& p2){
-    if (p1.angle < p2.angle) return false;
-    else if (p1.angle == p2.angle){
+    if (p1.y*p2.x < p1.x*p2.y) return false;
+    else if (p1.y*p2.x == p2.y*p1.x){
         if(p1.x == p2.x)
             return p1.y > p2.y;
         else return p1.x > p2.x;
     }
     else return true;
-}
-
-// Calculate arctan from origin
-double calcAtan(const Point& point){
-    if(point.x == 0){
-        if(point.y==0)
-            return PI/2 +1; // 최댓값을 리턴하게함
-        else if(point.y<0)
-            return PI/2;
-        else return -PI/2;
-    }
-    else return atan((double)point.y/point.x);
 }
 
 double CCW(Point& start, Point& end, Point& np){
@@ -54,16 +34,22 @@ double CCW(Point& start, Point& end, Point& np){
     return ((v1_x*v2_y)-(v1_y*v2_x));
 }
 
+void printpoints(Point* points, int N){
+    for(int i =0 ; i<N; i++){
+        cout << "( "<<points[i].x <<" , "<<points[i].y << " )"<< endl;
+    }
+}
+
 int main (){
     int N;
     cin >> N;
     stack <int> shell1;
     stack <int> shell2;
     int max_x_id;
-    long long max_x= -40001;
-    long long m_y;
+    long long max_x = -40001;
+    long long m_y = -40001;
+
     int count = 2;
-    int count_on = 0;
     vector<Point> points;
     
     for (int i = 0; i< N; i++){
@@ -71,25 +57,27 @@ int main (){
         int a, b;
         cin >> a >> b;
         point.setpoint(a,b);
-        if(a>=max_x){
+        if(a > max_x){
             max_x_id = i;
             max_x = a;
             m_y = b;
         }
+        else if(a==max_x){
+            if(b>m_y){
+                max_x_id = i;
+                max_x = a;
+                m_y = b;
+            }
+        }
         points.push_back(point);
     }
-    // Set max_x point to (0,0) & calculate Atan
+    // Set max_x point to (0,0)
     for(int i = 0; i< N; i++){
         points[i].setpoint(points[i].x-max_x, points[i].y-m_y);
-        points[i].setAtan(calcAtan(points[i]));
     }
     
-    // Sort points by atan value
+    // Sort points by angle
     sort(points.begin(), points.end(), comp);
-
-    for(int i =0 ; i<N; i++){
-        cout << "( "<<points[i].x <<" , "<<points[i].y << " )"<< endl;
-    }
 
     shell1.push(0);
     shell2.push(0);
@@ -126,22 +114,6 @@ int main (){
         shell2.pop();
         shell1.pop();
         count--;
-    }
-    // 여기에서 points[1]이 아니고 shell2[1]이 되어야 정확함
-/*    else if(CCW(points[N-1],points[0],points[1])==0){
-        count--;
-    }*/
-
-    while(!shell2.empty()){
-        idx2 = shell2.top();
-        cout << "idx2: "<< idx2<<" ("<<points[idx2].x<<" , "<< points[idx2].y << ")" << endl;
-        if(idx2!=0 && points[idx2].x == 0)
-            count_on ++;
-        shell2.pop();
-    }
-
-    if(count_on>0){
-        count = count-count_on +1;
     }
 
     cout << count;
