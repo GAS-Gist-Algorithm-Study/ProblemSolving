1bf7233395fecd93cc12c88451f37c6165ea7c06
Yura Choi <yoorachoi8937@gmail.com>
2020-03-01 20:20:01 +0900

    Help me solve 1647

--- PS_1647_CITY_PLAN/YRC/1647.cpp
+++ PS_1647_CITY_PLAN/YRC/1647.cpp
@@ -1,31 +1,77 @@
 #include <iostream>
+#include <vector>
 #include <queue>
+#include <algorithm>
 
 using namespace std;
 
-typedef pair<int,int> pii;
-typedef pair<int,pii> piii;
-int N, M;
+int id[100001];
+int totalcost;
+int last_cost;
+int M, N;
 
-class Comp{
-    public:
-    bool operator()(piii p1, piii p2){
-        return p1.first <= p2.first;
-    }
+struct Edge{
+    int V1;
+    int V2;
+    int cost;
 };
 
-priority_queue<piii, vector<piii>, Comp> map;
 
-void KMP(){
-    while
+
+//priority_queue<Edge, vector<Edge>, Comp> PQ;
+vector<Edge> PQ;
+int find_id(int i){
+    if (i == id[i])
+        return i;
+    return id[i] = find_id(id[i]);
+}
+
+void set_union(int a, int b) {
+  int A = find_id(a);
+  int B = find_id(b);
+  if (B < A)
+    id[A] = B;
+  else 
+    id[B] = A;
+}
+
+void Kruskal(){
+    int count = 0;
+    sort(PQ.begin(), PQ.end(), 
+         [](Edge& E1, Edge& E2){
+        return E1.cost < E2.cost;
+    }
+        );
+    for(auto e: PQ){       
+        if(count == N-1){
+            break;
+        }
+        
+        if(find_id(e.V1) != find_id(e.V2)){
+            set_union(e.V1,e.V2);
+            //cout << "id of V1 (" << front.V1 << "): " << id[front.V1] << ", id of V2 (" << front.V2 << "): " << id[front.V2] << endl;
+            last_cost = e.cost;
+            totalcost = totalcost + last_cost;
+            count++;
+        }
+    }  
 }
 
 int main (){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    Edge edge;
+    int cost, a, b;
     cin >> N >> M;
-    piii house;
-    for(int i = 0; i<M; i++){
-        cin >> house.second.first >> house.second.second >> house.first;
-        map.push(house);
+
+    for(int i =0 ; i<M; i++){
+        cin >> edge.V1 >> edge.V2 >> edge.cost;
+        PQ.push_back(edge);
+        id[i] = i;
     }
 
+    Kruskal();
+    
+    totalcost = totalcost - last_cost;
+    cout << totalcost;
 }
