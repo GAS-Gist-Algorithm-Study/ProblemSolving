ffb71d7c303c32c771fad93fc4baf6ed2fb295a4
Jonghun Park <jonghun.park.194@gmail.com>
2020-03-08 20:57:15 +0900

    Solve Travle problem (#2157)

---
+++ PS_2157_TRAVEL/PJH/2157.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <algorithm>
+#include <utility>
+#include <vector>
+
+typedef std::pair<int, int> edge;
+#define point first
+#define src second
+#define IMPOSIBBLE -2
+
+using namespace std;
+
+int findMaxRoute(
+    const vector<edge> nodes[], 
+    int** lut, 
+    int N, int M
+    )
+{
+  if (lut[N][M] != -1)
+    return lut[N][M];
+
+  if (M == 2) {
+    int maxPoint = -1;
+
+    for (const edge& e: nodes[N])
+      if (e.src == 1 && e.point > maxPoint)
+        maxPoint = e.point;
+
+    if (maxPoint == -1)
+      return lut[N][M] = IMPOSIBBLE;
+
+    return lut[N][M] = maxPoint;
+  } else {
+    int maxPoint = -1;
+    
+    for (const edge& e: nodes[N]) {
+      int partialMax = findMaxRoute(nodes, lut, e.src, M - 1);
+      if (partialMax == IMPOSIBBLE)
+        continue;
+
+      int partialSum = partialMax + e.point;
+      if (partialSum > maxPoint)
+        maxPoint = partialSum;
+    }
+    if (maxPoint == -1)
+      return lut[N][M] = IMPOSIBBLE;
+
+    return lut[N][M] = maxPoint;
+  }
+}
+
+int findMaxRoute(const vector<edge> nodes[], int N, int M)
+{
+  int** lut = (int**) malloc(sizeof(int*) * (N + 1));
+  for (int i = 0; i <= N; i++)
+    lut[i] = (int*) malloc(sizeof(int) * (M + 1));
+  
+  for (int i = 0; i <= N; i++)
+    for (int j = 0; j <= M; j++)
+      lut[i][j] = -1;
+
+  for (int j = 2; j <= N; j++)
+    lut[j][1] = lut[j][0] IMPOSIBBLE;
+
+  for (int i = 1; i <= M; i++)
+    lut[1][i] = 0;
+
+  lut[0][0] = IMPOSIBBLE;
+  lut[1][0] = IMPOSIBBLE;
+  
+  return findMaxRoute(nodes, lut, N, M);
+}
+
+int main()
+{
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int N, M, K;
+  cin >> N >> M >> K;
+
+  vector<edge> nodes[N + 1];
+  for (int k = 0; k < K; k++) {
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    if (a < b) 
+      nodes[b].emplace_back(c, a);
+  }
+
+  cout << findMaxRoute(nodes, N, M) << '\n';
+  return 0;
+}
